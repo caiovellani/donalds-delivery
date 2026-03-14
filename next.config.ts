e22e331307aae82f666032f088c9cba3c9c028fd
Caio Vellani <caiovellani@outlook.com>
2026-03-14 16:27:55 -0300

    chore: add images on project

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'u9a6wmr3as.ufs.sh',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
